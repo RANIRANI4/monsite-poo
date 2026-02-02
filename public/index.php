d858c1dc96e7e1172d30b4a7ea8eebd85743ce39
RANIRANI4 <rani.kabbouri4@gmail.com>
2026-02-02 15:58:59 +0100

    config front controller

--- public/index.php
+++ public/index.php
@@ -1,16 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
-</head>
-<body>
-<h2>
-    voici h2
-</h2>
-<h1>
-    rr
-</h1>
-</body>
-</html>
+<?php
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+use App\Core\App;
+
+App::run();
+
